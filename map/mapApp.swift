e7e73f8c3c1d9aa3dc6b72ccc5de7c566f44bf66
SHYA95 <yassershrouk254@gmail.com>
2023-03-19 13:19:25 +0200

    Initial Commit

---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by Shrouk Yasser on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
